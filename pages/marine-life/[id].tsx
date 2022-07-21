9a0755fdc1c01808cdb2d27fcec63e1afc914e3a
Louis THOMAS <louisthomas.pro@gmail.com>
2022-07-21 12:29:02 +0200

    Add marine-life dynamic route

---
+++ pages/marine-life/[id].tsx
@@ -0,0 +1,70 @@
+import { ImageList, ImageListItem, Typography } from "@mui/material";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { getLife } from "../../firebase/life.firestore";
+
+const MarineLife: NextPage<{ marineLifeData: any }> = ({ marineLifeData }) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading marine life...</div>;
+  }
+
+  return (
+    <>
+      {/* <BackButton />
+      <FavoriteButton /> */}
+      <Typography component="h1">
+        {marineLifeData.french_common_name}
+      </Typography>
+      <Typography
+        variant="caption"
+        color="text.secondary"
+        sx={{ fontStyle: "italic" }}
+      >
+        {marineLifeData.scientific_name}
+      </Typography>
+      <Link
+        href={marineLifeData.wikipedia_url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Wikipedia
+      </Link>
+
+      <ImageList cols={3} rowHeight={164}>
+        {marineLifeData.photos.map((photo: any) => (
+          <ImageListItem key={photo.id}>
+            <Image
+              loader={() => photo.medium_url}
+              src="/loading-fish.webp" // not working ?
+              layout="fill"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { id } = context.params!;
+  const marineLifeData = await getLife(id!.toString());
+
+  if (marineLifeData) {
+    return { props: { marineLifeData } };
+  } else {
+    return { notFound: true };
+  }
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // get all life ids...
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export default MarineLife;
